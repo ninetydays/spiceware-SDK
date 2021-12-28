4a53785f5eb44135b261e760389d1b068c10dd51
JonghunYu <yuzong@naver.com>
2021-12-28 17:37:29 +0900

    chore: exlude files from package

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.ts",
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: "ts-loader",
-        exclude: /node_modules/,
+        exclude: [/node_modules/],
       },
     ],
   },
@@ -15,7 +16,7 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "bundle.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
